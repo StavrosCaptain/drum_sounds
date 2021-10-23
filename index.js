d6c0d76c1940c557bcabb273991655d403ff2178
CaptainStavr <kali@CaptainStavr.localdomain>
2021-10-23 20:26:50 +0300

    playing drums

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+
+}
+
+
+
+
+function handleClick() {
+    alert("alerted from function");
+}
